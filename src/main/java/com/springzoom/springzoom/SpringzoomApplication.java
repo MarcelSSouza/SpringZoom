b1698b31cabde89df3830435b88e04a3d50e68de
Marcel de Araújo Santos Souza <marcelaraujosantossouza19@gmail.com>
2023-05-07 12:29:02 +0100

    Initial Commit

---
+++ src/main/java/com/springzoom/springzoom/SpringzoomApplication.java
@@ -0,0 +1,13 @@
+package com.springzoom.springzoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringzoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringzoomApplication.class, args);
+	}
+
+}
