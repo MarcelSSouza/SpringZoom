4c96f0b5ab8e51b87cc5bd1ccfa4b4461f4e8072
Marcel de Araújo Santos Souza <marcelaraujosantossouza19@gmail.com>
2023-05-22 15:56:23 +0100

    Finish

--- springzoom_react/src/Components/Home.jsx
+++ springzoom_react/src/Components/Home.jsx
@@ -10,34 +10,12 @@ import LogoutButton from "./LogoutButton";
 
 function Home() {
   const [meetings, setMeetings] = useState([]);
-  const [selectedMeeting, setSelectedMeeting] = useState(null); // New state variable to track selected meeting
+  const [selectedMeeting, setSelectedMeeting] = useState(null);
   const storedUser = sessionStorage.getItem("user");
   const user = storedUser ? JSON.parse(storedUser) : null;
   const [editingMeeting, setEditingMeeting] = useState(null);
-
-  const handleEditClick = (meeting) => {
-    setEditingMeeting(meeting);
-    setSelectedMeeting(null); // Reset the selected meeting when editing starts
-  };
-  const handleSaveChanges = () => {
-    axios
-      .put(`http://localhost:8080/meetings/${editingMeeting.meetingId}`, editingMeeting)
-      .then((response) => {
-        alert("Meeting updated successfully!");
-        setEditingMeeting(null);
-        fetchMeetingsByEmail(user.email);
-      })
-      .catch((error) => {
-        alert("Failed to update meeting: " + error.response.data.message);
-      });
-  };
-  
-
-  const handleDetailsClick = (meeting) => {
-    setSelectedMeeting(meeting);
-    setEditingMeeting(null); // Reset the editing state when details are shown
-  };
-
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   useEffect(() => {
     if (!!user) {
@@ -56,10 +34,43 @@ function Home() {
       });
   };
 
+  const handleEditClick = (meeting) => {
+    setEditingMeeting(meeting);
+    setSelectedMeeting(null);
+  };
 
+  const handleSaveChanges = () => {
+    axios
+      .put(`http://localhost:8080/meetings/${editingMeeting.meetingId}`, editingMeeting)
+      .then((response) => {
+        alert("Meeting updated successfully!");
+        setEditingMeeting(null);
+        fetchMeetingsByEmail(user.email);
+      })
+      .catch((error) => {
+        alert("Failed to update meeting: " + error.response.data.message);
+      });
+  };
+
+  const handleDetailsClick = (meeting) => {
+    setSelectedMeeting(meeting);
+    setEditingMeeting(null);
+  };
 
   const handleCloseCard = () => {
     setSelectedMeeting(null);
+  };
+
+  const filterMeetings = () => {
+    const filtered = meetings.filter((meeting) => {
+      const meetingDateTime = new Date(meeting.meetingDate + " " + meeting.meetingTime);
+      const startDateTime = new Date(startDate);
+      const endDateTime = new Date(endDate);
+
+      return meetingDateTime >= startDateTime && meetingDateTime <= endDateTime;
+    });
+
+    setMeetings(filtered);
   };
 
   return (
@@ -73,8 +84,7 @@ function Home() {
               <Nav.Link href="/home">Home</Nav.Link>
               <Nav.Link href="/contacts">Contacts</Nav.Link>
               <Nav.Link href="/newmeeting">Add Meeting</Nav.Link>
-<LogoutButton />
-
+              <LogoutButton />
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -83,9 +93,31 @@ function Home() {
         <h2>Welcome to SpringZoom {user.name}</h2>
         <h2>Your id is {user.id}</h2>
         <h3>
-          SpringZoom is a web application that allows you to schedule meetings
-          with your contacts.
+          SpringZoom is a web application that allows you to schedule meetings with your contacts.
         </h3>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicStartDate">
+            <Form.Label>Start Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Start Date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEndDate">
+            <Form.Label>End Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="End Date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </Form.Group>
+          <button type="button" onClick={filterMeetings}>
+            Filter
+          </button>
+        </Form>
         <Table striped bordered>
           <thead>
             <tr>
@@ -113,18 +145,13 @@ function Home() {
                   <button
                     onClick={() => {
                       axios
-                        .delete(
-                          `http://localhost:8080/meetings/delete/${meeting.meetingId}`
-                        )
+                        .delete(`http://localhost:8080/meetings/delete/${meeting.meetingId}`)
                         .then((response) => {
                           alert("Meeting deleted successfully!");
                           fetchMeetingsByEmail(user.email);
                         })
                         .catch((error) => {
-                          alert(
-                            "Failed to delete meeting: " +
-                              error.response.data.message
-                          );
+                          alert("Failed to delete meeting: " + error.response.data.message);
                         });
                     }}
                   >
@@ -132,12 +159,10 @@ function Home() {
                   </button>
                 </td>
                 <td>
-                <button onClick={() => handleEditClick(meeting)}>Edit</button>
+                  <button onClick={() => handleEditClick(meeting)}>Edit</button>
                 </td>
                 <td>
-                  <button onClick={() => handleDetailsClick(meeting)}>
-                    Details
-                  </button>
+                  <button onClick={() => handleDetailsClick(meeting)}>Details</button>
                 </td>
               </tr>
             ))}
@@ -149,7 +174,7 @@ function Home() {
             <Card.Body>
               <Card.Title>Meeting Details</Card.Title>
               <Card.Text>
-              <strong>Title:</strong> {selectedMeeting.title}
+                <strong>Title:</strong> {selectedMeeting.title}
                 <br />
                 <strong>Email 1:</strong> {selectedMeeting.email1}
                 <br />
@@ -159,71 +184,78 @@ function Home() {
                 <br />
                 <strong>Time:</strong> {selectedMeeting.meetingTime}
                 <br />
-                {/* Add additional details as needed */}
               </Card.Text>
               <button onClick={handleCloseCard}>Close</button>
             </Card.Body>
           </Card>
         )}
-         {
-editingMeeting && (
-<Card>
-<Card.Header>Edit Meeting</Card.Header>
-<Card.Body>
-    <Form className="d-flex flex-column">
-    <Form.Group className="mb-3" controlId="formBasicTitle">
-  <Form.Label>Title</Form.Label>
-  <Form.Control
-    type="title"
-    placeholder="Title"
-    value={editingMeeting.title}
-    onChange={(e) => setEditingMeeting({...editingMeeting, title: e.target.value})}
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="formBasicDate">
-  <Form.Label>Date</Form.Label>
-  <Form.Control
-    type="Date"
-    placeholder="Date"
-    value={editingMeeting.meetingDate}
-    onChange={(e) => setEditingMeeting({...editingMeeting, meetingDate: e.target.value})}
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="formBasicHour">
-  <Form.Label>Hour</Form.Label>
-  <Form.Control
-    type="Time"
-    placeholder="Hour"
-    value={editingMeeting.meetingTime}
-    onChange={(e) => setEditingMeeting({...editingMeeting, meetingTime: e.target.value})}
-  />
-</Form.Group>
-<Form.Group className="mb-3" controlId="formBasicEmail">
-  <Form.Label>Your email</Form.Label>
-  <Form.Control
-    type="email"
-    placeholder="Enter email"
-    value={editingMeeting.email1}
-    onChange={(e) => setEditingMeeting({...editingMeeting, email1: e.target.value})}
-  />
-  <Form.Text className="text-muted"></Form.Text>
-</Form.Group>
-<Form.Group className="mb-3" controlId="formBasicEmail">
-  <Form.Label>Email of the other user</Form.Label>
-  <Form.Control
-    type="email"
-    placeholder="Enter email"
-    value={editingMeeting.email2}
-    onChange={(e) => setEditingMeeting({...editingMeeting, email2: e.target.value})}
-  />
-  <Form.Text className="text-muted"></Form.Text>
-</Form.Group>
-<button onClick={handleSaveChanges}>Save Changes</button>
-
-        </Form>
-</Card.Body>
-</Card>
-)}
+        {editingMeeting && (
+          <Card>
+            <Card.Header>Edit Meeting</Card.Header>
+            <Card.Body>
+              <Form className="d-flex flex-column">
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    type="title"
+                    placeholder="Title"
+                    value={editingMeeting.title}
+                    onChange={(e) =>
+                      setEditingMeeting({ ...editingMeeting, title: e.target.value })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicDate">
+                  <Form.Label>Date</Form.Label>
+                  <Form.Control
+                    type="date"
+                    placeholder="Date"
+                    value={editingMeeting.meetingDate}
+                    onChange={(e) =>
+                      setEditingMeeting({ ...editingMeeting, meetingDate: e.target.value })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicHour">
+                  <Form.Label>Hour</Form.Label>
+                  <Form.Control
+                    type="time"
+                    placeholder="Hour"
+                    value={editingMeeting.meetingTime}
+                    onChange={(e) =>
+                      setEditingMeeting({ ...editingMeeting, meetingTime: e.target.value })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Your email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={editingMeeting.email1}
+                    onChange={(e) =>
+                      setEditingMeeting({ ...editingMeeting, email1: e.target.value })
+                    }
+                  />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email of the other user</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={editingMeeting.email2}
+                    onChange={(e) =>
+                      setEditingMeeting({ ...editingMeeting, email2: e.target.value })
+                    }
+                  />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
+                <button onClick={handleSaveChanges}>Save Changes</button>
+              </Form>
+            </Card.Body>
+          </Card>
+        )}
       </Container>
     </>
   );
